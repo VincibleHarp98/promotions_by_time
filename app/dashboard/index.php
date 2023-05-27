370441a649fd42f5cfa52d092cb9a6475e9ec0fa
VincibleHarp98 <49323554+VincibleHarp98@users.noreply.github.com>
2023-05-27 11:48:48 -0600

    dashboard de administrador fase 2

--- app/dashboard/index.php
+++ app/dashboard/index.php
@@ -1,4 +1,14 @@
-
+<?php
+/*  session_start();
+if (isset($_SESSION['usuario'])) {
+    if ($_SESSION['usuario']['roll'] != "1"){
+      header("Location: ../distribuidor/");
+    }
+}else {
+  header('Location: ../../');
+}
+*/
+ ?>
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
   <head>
@@ -15,8 +25,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    <link rel="icon" type="image/png" href="../../assets/icon/logo.png" />
-    <title>Control Taller</title>
+    <link rel="icon" type="image/png" href="../../img/logo.png" />
+    <title>Promotions_by_time</title>
   </head>
   <body>
     <div id="contenedor_cargar">
@@ -33,7 +43,7 @@
     <div class="sidebar">
    <div class="logo-details">
      <i class='bx bxl-c-plus-plus icon'><i class="fas fa-hand-holding-water"></i></i>
-       <div class="logo_name">Control</div>
+       <div class="logo_name">CT Promos</div>
        <i class='bx bx-menu' id="btn" > <i class="fas fa-align-right"></i></i>
    </div>
    <ul class="nav-list">
@@ -45,30 +55,31 @@
      <li>
        <a href="./">
          <i class="fas fa-home"></i>
-         <span class="links_name">Tareas Activas</span>
+         <span class="links_name">Tickets Activados</span>
        </a>
-        <span class="tooltip">Tareas Activas</span>
+        <span class="tooltip">Tickets Activados</span>
      </li>
      <li>
       <a href="./kms/">
       <i class="fas fa-solid fa-compass"></i>
-        <span class="links_name">Kilometrajes</span>
+        <span class="links_name">Crear Promo</span>
       </a>
-      <span class="tooltip">Kilometrajes</span>
+      <span class="tooltip">Crear Promo</span>
     </li>
      <li>
        <a href="./fechas/">
        <i class="fas fa-solid fa-file-excel"></i>
-         <span class="links_name">Fechas</span>
+         <span class="links_name">Ver Promos</span>
        </a>
-        <span class="tooltip">Fechas</span>
+        <span class="tooltip">Ver Promos</span>
      </li>
+     <!--
      <li>
        <a href="./fechaunidades/">
        <i class="fas fa-solid fa-file-excel"></i>
-         <span class="links_name">Fechas Y Unidad</span>
+         <span class="links_name">Ver Clientes</span>
        </a>
-        <span class="tooltip">Fechas Y Unidad</span>
+        <span class="tooltip">Ver Cleintes</span>
      </li>
      <li>
        <a href="./fechamecanicos/">
@@ -84,14 +95,15 @@
       </a>
       <span class="tooltip">Catalogos</span>
     </li>
+    -->
     <li>
        
     <li>
         <a href="./llantas-storage/">
         <i class="fas fa-solid fa-tire"></i>
-          <span class="links_name">Control de Llantas</span>
+          <span class="links_name">Ver Clientes</span>
         </a>
-        <span class="tooltip">Control Llantas</span>
+        <span class="tooltip">Ver Clientes</span>
       </li>
  <!--
     <li> 
@@ -143,26 +155,7 @@
    </ul>
  </div>
  <section class="home-section">
-    <br>
-      
    
-    
-
-     <div class="container-fluid">
-       <h1 style="text-align:center;">Tareas Activas</h1>
-       <br>
-         <div class="row">
-                 <div class="col-lg-12">
-                     <div class="table-responsive">
-
-                         <div id="resultado"></div>
-                     </div>
-                 </div>
-         </div>
-     </div>
-
-     </div>
-
  </section>
  <script>
  let sidebar = document.querySelector(".sidebar");
@@ -187,7 +180,7 @@
     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
   }
  }
-
+/*
  $(document).ready(function() {
      //mostrar datos
      function obtener_datos(){
@@ -204,7 +197,7 @@
 
     //console.log(output);
  });
-
+*/
 
  function ajustecuenta(){
   Swal.fire("Esperalo muy pronto");
